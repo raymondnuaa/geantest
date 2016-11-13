7938e534eee69761518456de90965ed9c7c48ac7
raymond <raymondnuaa@gmail.com>
2016-11-13 23:51:00 +0800

    add online import part

--- OrdersOnlineImport.py
+++ OrdersOnlineImport.py
@@ -2,13 +2,57 @@
 import random
 import unittest
 
+import time, os
+import traceback
+from datetime import datetime    
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
 class TestOrdersOnlineImport(unittest.TestCase):
+    url    = 'http://115.29.249.35:9999/'
+    driver = webdriver.Chrome('C:\Python27\chromedriver.exe')
+    
     def setUp(self):
         pass
         
     def test_B1_ImportSettings(self):
-        pass 
+        driver = self.driver
+        try:
+            driver.get(self.url)
+            time.sleep(3)
+        except Exception as e:        
+            print(traceback.format_exc())
+            self.driver.quit()
+            
+        elem = driver.find_element_by_id('u5')
+        elem.click()
+        time.sleep(3)
+        
+        select = Select(driver.find_element_by_id("u651_input"))        
+        select.select_by_visible_text("易尊商城")        
+        time.sleep(1)
+        
+        select = Select(driver.find_element_by_id("u665_input"))        
+        select.select_by_visible_text("预约导入")        
+        time.sleep(1)
+        
+        
+        elem = driver.find_element_by_id('u657_input')
+        elem.send_keys('2016-11-13 23:25:25')
+        time.sleep(1)
+        
+        elem = driver.find_element_by_id('u661')
+        elem.click()
+        time.sleep(3)
+        
+        #return driver.alert_is_present
+        alert = driver.switch_to.alert
+        elem  = alert.find_element_by_id('alert-title')
+        print elem
+        
     
     def test_B2_OnlineImport(self):    
+        self.driver.quit()
         pass
 
--- suitTest.py
+++ suitTest.py
@@ -13,8 +13,8 @@ from PassFailErrorExample import TestPassFailErrorExample
 
 if __name__ == '__main__':    
     
-    suite1 = unittest.TestLoader().loadTestsFromTestCase(TestOrdersFileImport)
-    #suite2 = unittest.TestLoader().loadTestsFromTestCase(TestOrdersOnlineImport)
+    #suite1 = unittest.TestLoader().loadTestsFromTestCase(TestOrdersFileImport)
+    suite2 = unittest.TestLoader().loadTestsFromTestCase(TestOrdersOnlineImport)
     #suite3 = unittest.TestLoader().loadTestsFromTestCase(TestOrdersCreate)
     #suite4 = unittest.TestLoader().loadTestsFromTestCase(TestOrdersQuery)
     #suite5 = unittest.TestLoader().loadTestsFromTestCase(TestOrdersReAssign)
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     
     #suite = unittest.TestSuite([suite1, suite2, suite3, suite4, suite5, suite6])
     
-    suite = unittest.TestSuite([suite1])
+    suite = unittest.TestSuite([suite2])
     
     #unittest.TextTestRunner(verbosity=2).run(suite)
     
